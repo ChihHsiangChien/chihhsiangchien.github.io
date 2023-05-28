7963ddf29ba9e47c5e7eec092b1c718d3aba00bf
pancala <pancala@gmail.com>
2023-05-28 17:23:42 +0800

    sound

--- cardGame/data/microscope.js
+++ cardGame/data/microscope.js
@@ -9,7 +9,7 @@ var cardData = [
   {"name":"紅血球","category":"複式顯微鏡"},
   {"name":"草履蟲","category":"複式顯微鏡"},
   {"name":"單胞藻","category":"複式顯微鏡"},
-  {"name":"昆布","category":"複式顯微鏡"},
+  {"name":"昆布","category":"解剖顯微鏡"},
   {"name":"花粉","category":"複式顯微鏡"},
   {"name":"柱頭","category":"解剖顯微鏡"},
   {"name":"子房","category":"解剖顯微鏡"},
