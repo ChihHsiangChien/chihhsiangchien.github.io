0721c35676b5bbb6475cf63a4cde684d6fc96416
ChihHsiangChien <pancala@gmail.com>
2023-05-30 10:52:47 +0800

    update css index.html memory

--- cardGame/其他_台灣行政區_配對/settings.js
+++ cardGame/其他_台灣行政區_配對/settings.js
@@ -2,8 +2,8 @@ cardsArrangementType = "grid";
 cardsX = 5;
 cardsY = 5;
 cardFillColor = "#2e4023";
-hSpace = 2;
-vSpace = 2;
+hSpace = 0.5;
+vSpace = 0.5;
 fontRatio = 0.27;
 gameType = "match";
 
--- cardGame/顯微鏡_哪些用顯微鏡觀察_分類/settings.js
+++ cardGame/顯微鏡_哪些用顯微鏡觀察_分類/settings.js
@@ -1,3 +1,7 @@
+cardWidth = 170;
+cardHeight = 60;
+answerBoxHeight = (canvas.height * 0.8) / answerBoxNumRows;
+cardsY = 20 + answerBoxHeight * answerBoxNumRows; // 待答卡片的最高高度，需要與answerBoxHeight 配
 gameType = "classify";
 
 readCardData();
