75566eb3b67613b6641d522b9b116937c7ae5fd9
ChihHsiangChien <pancala@gmail.com>
2023-06-02 15:07:16 +0800

    update ecogame

--- ecologicalGame/script.js
+++ ecologicalGame/script.js
@@ -3,16 +3,16 @@ class Organism {
     this.type = type; // 生物的類型
     this.x = x; // 位置的x坐标
     this.y = y; // 位置的y坐标
-    this.xGridIdx = Math.ceil(this.x / gridWidth);
-    this.yGridIdx = Math.ceil(this.y / gridHeight);
-    this.lifespan = lifespan;    // 壽命
-    this.energy = 100;         // 吃飽程度
+    this.xGridIdx = Math.floor(this.x / gridWidth);
+    this.yGridIdx = Math.floor(this.y / gridHeight);
+    this.lifespan = lifespan; // 壽命
+    this.energy = 100; // 吃飽程度
     this.maxLifespan = lifespan; // 最大生命值
 
     this.xSpeed = xSpeed; // 速度
     this.ySpeed = ySpeed; // 速度
     this.image = new Image();
-    this.image.src = this.type + ".png"
+    this.image.src = this.type + ".png";
 
     this.numChildren = 0;
 
@@ -31,14 +31,22 @@ Organism.prototype.draw = function () {
   const lifespanWidth = (this.lifespan / this.maxLifespan) * imageWidth;
   ctx.fillRect(this.x, this.y + imageHeight, lifespanWidth, 5);
 
+  if (this.type === "grass") {
+    return;
+  }
   // 繪製energy條
   ctx.fillStyle = "blue";
   const energyWidth = (this.energy / 100) * imageWidth;
   ctx.fillRect(this.x, this.y + imageHeight * 1.2, energyWidth, 5);
 };
 
-Organism.prototype.move = function () {  
-  const gridIndex = grids[this.type][this.yGridIdx][this.xGridIdx].indexOf(this);
+Organism.prototype.move = function () {
+  if (this.type === "grass") {
+    return;
+  }
+  const gridIndex =
+    grids[this.type][this.yGridIdx][this.xGridIdx].indexOf(this);
+
   if (gridIndex !== -1) {
     grids[this.type][this.yGridIdx][this.xGridIdx].splice(gridIndex, 1);
   }
@@ -47,6 +55,15 @@ Organism.prototype.move = function () {
   this.x += this.xSpeed;
   this.y += this.ySpeed;
 
+  // 检测邊界碰撞
+  if (this.x <= gridWidth || this.x >= canvas.width) {
+    this.xSpeed *= -1; // 改變x方向速度
+  }
+
+  if (this.y <= gridHeight || this.y >= canvas.height) {
+    this.ySpeed *= -1; // 改變y方向速度
+  }
+
   this.xGridIdx = Math.floor(this.x / gridWidth);
   this.yGridIdx = Math.floor(this.y / gridHeight);
 
@@ -54,56 +71,54 @@ Organism.prototype.move = function () {
     grids[this.type][this.yGridIdx][this.xGridIdx] = [];
   }
   grids[this.type][this.yGridIdx][this.xGridIdx].push(this);
-
-  // 检测邊界碰撞
-  if (this.x <= 0 || this.x + imageWidth >= canvas.width) {
-    this.xSpeed *= -1; // 改變x方向速度
-  }
-
-  if (this.y <= 0 || this.y + imageHeight * 1.5 >= canvas.height) {
-    this.ySpeed *= -1; // 改變y方向速度
-  }
 };
 
 Organism.prototype.eat = function () {
-  if (this.type !== "grass") {
-    grids["grass"][this.yGridIdx][this.xGridIdx]
-
+  if (this.type === "grass") {
+    return;
   }
-
-}
+  if (grids["grass"][this.yGridIdx][this.xGridIdx] === null) {
+    return;
+  }
+  console.log(grids["grass"][this.yGridIdx][this.xGridIdx]);
+  if (grids["grass"][this.yGridIdx][this.xGridIdx].length !== 0) {
+    this.energy += 10;
+    var food = grids["grass"][this.yGridIdx][this.xGridIdx].pop();
+    food.dead();
+  }
+};
 
 Organism.prototype.dead = function () {
   const index = organisms[this.type].indexOf(this);
   if (index !== -1) {
     organisms[this.type].splice(index, 1);
   }
-  const gridIndex = grids[this.type][this.yGridIdx][this.xGridIdx].indexOf(this);
+  const gridIndex =
+    grids[this.type][this.yGridIdx][this.xGridIdx].indexOf(this);
   if (gridIndex !== -1) {
     grids[this.type][this.yGridIdx][this.xGridIdx].splice(gridIndex, 1);
   }
-}
+};
 
 function createGrid(type) {
   if (!grids.hasOwnProperty(type)) {
-    var matrix = new Array(numGridY).fill(null).map(() => new Array(numGridX).fill(null));
+    var matrix = new Array(numGridY)
+      .fill(null)
+      .map(() => new Array(numGridX).fill(null));
     //var matrix = new Array(numGridY).fill(0).map(() => new Array(numGridX).fill(0));
-    grids[type] = matrix;    
+    grids[type] = matrix;
   }
 }
 function getEmptyCell(type) {
-
-  var xGridIdx = Math.floor(Math.random() * canvas.width / gridWidth);
-  var yGridIdx = Math.floor(Math.random() * canvas.height / gridHeight);
+  var xGridIdx = Math.floor((Math.random() * canvas.width) / gridWidth);
+  var yGridIdx = Math.floor((Math.random() * canvas.height) / gridHeight);
 
   while (grids[type][yGridIdx][xGridIdx] !== null) {
-
     // console.log(grids[type][x][y]);
-    xGridIdx = Math.floor(Math.random() * canvas.width / gridWidth);
-    yGridIdx = Math.floor(Math.random() * canvas.height / gridHeight);
+    xGridIdx = Math.floor((Math.random() * canvas.width) / gridWidth);
+    yGridIdx = Math.floor((Math.random() * canvas.height) / gridHeight);
   }
   return { xGridIdx, yGridIdx };
-
 }
 Organism.prototype.born = function () {
   var { xGridIdx, yGridIdx } = getEmptyCell(this.type);
@@ -117,54 +132,41 @@ Organism.prototype.born = function () {
     xSpeed = getRandomInt(-5, 5);
     ySpeed = getRandomInt(-5, 5);
   }
-  var child = new Organism(
-    this.type,
-    x,
-    y,
-    this.maxLifespan,
-    xSpeed,
-    ySpeed
-  );
+  var child = new Organism(this.type, x, y, this.maxLifespan, xSpeed, ySpeed);
   if (organisms.hasOwnProperty(this.type)) {
-    organisms[this.type].push(child);
+    organisms[child.type].push(child);
   } else {
-    organisms[this.type] = [child];
+    organisms[child.type] = [child];
   }
 
-  xGridIdx = Math.floor(this.x / gridWidth);
-  yGridIdx = Math.floor(this.y / gridHeight);
-
-  if (!grids[this.type][this.yGridIdx][this.xGridIdx]) {
-    grids[this.type][this.yGridIdx][this.xGridIdx] = [];
+  if (!grids[child.type][child.yGridIdx][child.xGridIdx]) {
+    grids[child.type][child.yGridIdx][child.xGridIdx] = [];
   }
-  grids[this.type][this.yGridIdx][this.xGridIdx].push(child);
-
+  grids[child.type][child.yGridIdx][child.xGridIdx].push(child);
 };
 
 Organism.prototype.updateAgeAndLifespan = function () {
+  switch (this.type) {
+    case "grass":
+      {
+        const timeFactor = 0.1; // 控制時間對壽命的影響程度
+        this.lifespan -= timeFactor;
+      }
+      break;
+    default: {
+      const timeFactor = 0.3; // 控制時間對壽命的影響程度
+      const energyFactor = 0.05;
 
-  // 檢查 organism 是否為草
-  if (this.type !== 'grass') {
-    // 根據吃飽程度調整壽命變化
-    const energyFactor = 100 - this.energy; // 越餓(energy越小)，壽命降低越快
-    const timeFactor = 0.1; // 控制時間對壽命的影響程度
-    this.lifespan -= energyFactor * timeFactor;
-
-    // 根据移動距離降低能量
-    const distance = Math.sqrt(this.xSpeed ** 2 + this.ySpeed ** 2);
-    this.energy -= distance * 0.1;
-
-  } else {
-    // 調整草的壽命變化
-    const timeFactor = 0.2; // 控制時間對壽命的影響程度
-    this.lifespan -= timeFactor;
+      // this.lifespan -= timeFactor - (this.energy * energyFactor);
+      this.lifespan -= timeFactor;
+    }
   }
 
   // 檢查是否死亡
   if (this.lifespan <= 0) {
     this.dead();
   }
-}
+};
 
 // 從min到max取亂數
 function getRandomInt(min, max) {
@@ -172,18 +174,15 @@ function getRandomInt(min, max) {
   return Math.random() * (max - min + 1) + min;
 }
 
-
 function generateOrganisms(type, num) {
   for (let i = 0; i < num; i++) {
     var xGridIdx, yGridIdx;
     if (type == "grass") {
       ({ xGridIdx, yGridIdx } = getEmptyCell(type));
-
     } else {
-      xGridIdx = Math.floor(Math.random() * canvas.width / gridWidth);
-      yGridIdx = Math.floor(Math.random() * canvas.height / gridHeight);
+      xGridIdx = Math.floor((Math.random() * canvas.width) / gridWidth);
+      yGridIdx = Math.floor((Math.random() * canvas.height) / gridHeight);
     }
-
 
     var x = xGridIdx * gridWidth;
     var y = yGridIdx * gridHeight;
@@ -197,8 +196,7 @@ function generateOrganisms(type, num) {
 
     if (type in organisms) {
       organisms[type].push(organism);
-    }
-    else {
+    } else {
       organisms[type] = [organism];
     }
 
@@ -226,7 +224,6 @@ function animate() {
     }
   });
 
-
   organismTypes.forEach((type) => {
     organisms[type].forEach((organism) => {
       organism.move();
@@ -234,25 +231,41 @@ function animate() {
       organism.draw();
       organism.updateAgeAndLifespan();
 
+      // 可以生殖的時機
+      switch (organism.type) {
+        case "grass":{
+          if(
+          organism.lifespan < organism.maxLifespan * 0.7 &&
+          organism.lifespan >= organism.maxLifespan * 0.3 &&
+          organism.numChildren == 0    
+          )
+          {
+            organism.born();
+            organism.numChildren += 1;            
+          }
 
-      //壽命
-      if (organism.lifespan < organism.maxLifespan * 0.6 &&
-        organism.lifespan >= organism.maxLifespan * 0.5 &&
-        organism.numChildren == 0
-      ) {
-        organism.born();
-        organism.numChildren += 1;
+        }
+        break;
+        default:{
+          if (
+            organism.lifespan < organism.maxLifespan * 0.7 &&
+            organism.lifespan >= organism.maxLifespan * 0.3 &&
+            organism.energy > 100 &&
+            organism.numChildren == 0    
+          ) {
+            organism.born();
+            organism.numChildren += 1;
+          }
+        }
       }
+
     });
   });
-
 
   requestAnimationFrame(animate);
 }
 
-
 // 创建canvas元素
-// const canvas = document.createElement('canvas');
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
@@ -274,6 +287,6 @@ var grids = {};
 createGrid("sheep");
 generateOrganisms("sheep", 3);
 createGrid("grass");
-generateOrganisms("grass", 30);
+generateOrganisms("grass", 10);
 
 animate();
