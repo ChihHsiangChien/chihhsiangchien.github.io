70237106a3af74a9f8cf7a93ac6e19a2a7a8c26a
ChihHsiangChien <pancala@gmail.com>
2023-06-09 12:01:41 +0800

    update ecosystem

--- ecosystem/Species.js
+++ ecosystem/Species.js
@@ -1,30 +1,61 @@
-
 // 定義物種類別
 class Species {
-    constructor(name, population, preyGrowthRate, preyPredationRate, predatorGrowthRate, predatorDeathRate) {
-      this.name = name; // 物種名稱
-      this.population = population; // 初始族群數量
-      this.preyGrowthRate = preyGrowthRate; // 當自己是獵物時，族群成長速率
-      this.preyPredationRate = preyPredationRate; // 當自己是獵物時的被捕食速率
-      this.predatorGrowthRate = predatorGrowthRate; // 當自己是捕食者時的族群成長速率
-      this.predatorDeathRate = predatorDeathRate; // 當自己是捕食者的死亡速率
-      this.preysOn = []; // 捕食的物種
+  constructor(name, population, growthRate, deathRate) {
+    this.name = name; // 物種名稱
+    this.population = population; // 初始族群數量
+    this.prePopulation = population;
+    this.growthRate = growthRate; // 族群成長速率
+    this.deathRate = deathRate; // 族群死亡速率
+    this.preysOn = []; // 捕食的物種
+    this.isPreyOf = []; // 被哪些物種捕食
+  }
+
+  // 設定自己被哪些物種捕食
+  setPreyOf(species) {
+    this.isPreyOf.push(species);
+  }
+
+  // 更新族群數量
+  // 自己是獵物時，下次的族群改變量 = preyGrowthRate *(自己的族群數量)  - preyPredationRate*(自己的族群數量)*(所有會吃自己的物種族群數量)
+  // 自己是捕食者時，下次的族群改變量 = predatorGrowthRate *(自己的族群數量)*(所有獵物的族群數量)  - predatorDeathRate*(自己的族群數量)
+  updatePopulation() {
+    let preyPopulationSum = 0; //獵物族群總數
+    let predatorPopulationSum = 0; //天敵族群總數
+    let preyChange = 0;
+    let predatorChange = 0;
+
+    // 獲取所有獵物的總族群數量
+    for (const preySpecies of this.preysOn) {
+      preyPopulationSum += preySpecies.prePopulation;
     }
-  
-    // 更新族群數量
-    updatePopulation(preys) {
-      let preyPopulation = 0;
-      let predatorPopulation = 0;
-      for (const prey of preys) {
-        preyPopulation += prey.population;
-        predatorPopulation += prey.preysOn.includes(this) ? prey.population : 0;
-      }
-  
-      const preyGrowth = this.preyGrowthRate * this.population; // 當自己是獵物時的成長量
-      const preyPredation = this.preyPredationRate * this.population * predatorPopulation; // 當自己是獵物時的被捕食量
-      const predatorGrowth = this.predatorGrowthRate * this.population * preyPopulation; // 當自己是捕食者時的成長量
-      const predatorDeath = this.predatorDeathRate * this.population; // 當自己是捕食者時的死亡量
-  
-      this.population += preyGrowth - preyPredation + predatorGrowth - predatorDeath;
+
+    // 獲取所有天敵的總族群數量
+    for (const predatorSpecies of this.isPreyOf) {
+      predatorPopulationSum += predatorSpecies.prePopulation;
+    }
+
+    // 自己是獵物時的族群改變量
+    if (this.preysOn.length > 0 && predatorPopulationSum >= 0) {
+      preyChange =
+        this.growthRate * this.prePopulation -
+        this.deathRate * this.prePopulation * predatorPopulationSum;
+    }
+    // 自己是捕食者時的族群改變量
+    if (this.isPreyOf.length > 0 && preyPopulationSum >= 0) {
+      predatorChange =
+        this.growthRate * this.prePopulation * preyPopulationSum -
+        this.deathRate * this.prePopulation;
+    }
+
+    // 更新族群数量
+    this.population += preyChange + predatorChange;
+
+    // 確保族群數量不為負數
+    if (this.population < 0) {
+      this.population = 0;
     }
   }
+  updatePrePopulation() {
+    this.prePopulation = this.population;
+  }
+}
--- ecosystem/script.js
+++ ecosystem/script.js
@@ -5,50 +5,63 @@ canvas.height = 300;
 
 
 // 創建物種實例
-const stoneLion = new Species("石虎", 100, 0.03, 0.001, 0.01, 0.003);
-const sikaDeer = new Species("梅花鹿", 200, 0.02, 0.002, 0.02, 0.001);
-const yellowWolf = new Species("黃鼠狼", 50, 0.02, 0.003, 0.03, 0.002);
-const wildRabbit = new Species("野兔", 150, 0.04, 0.002, 0.02, 0.001);
-const grass = new Species("草", 500, 0.1, 0, 0.05, 0);
-const ringNeckPheasant = new Species("環頸雉", 80, 0.02, 0.001, 0.01, 0.001);
-const nestRat = new Species("巢鼠", 100, 0.02, 0.004, 0.01, 0.002);
-const squirrel = new Species("松鼠", 80, 0.03, 0.002, 0.02, 0.001);
-const snake = new Species("蛇", 50, 0.03, 0.005, 0.01, 0.001);
-const eagle = new Species("老鷹", 30, 0.02, 0.005, 0.02, 0.003);
+const leopardCat = new Species("石虎", 30, 0.02, 0.01);
+const eagle = new Species("老鷹", 30, 0.02, 0.01);
+const snake = new Species("蛇", 30, 0.02, 0.01);
+const weasel = new Species("黃鼠狼", 50, 0.03, 0.01);
+const rat = new Species("巢鼠", 100, 0.03, 0.01);
+const squirrel = new Species("松鼠", 80, 0.03, 0.02);
+const pheasant = new Species("環頸雉", 80, 0.03, 0.01);
+const sikaDeer = new Species("梅花鹿", 200, 0.02, 0.01);
+const rabbit = new Species("野兔", 150, 0.04, 0.03);
+const grass = new Species("草", 50000, 0.1, 0.01);
 
 // 更新物種間的捕食關係
-stoneLion.preysOn = [sikaDeer, yellowWolf, wildRabbit];
+
+// 設定獵物
+leopardCat.preysOn = [sikaDeer, weasel, rabbit];
+eagle.preysOn = [rat, pheasant, rabbit, squirrel];
+snake.preysOn = [rat, squirrel, rabbit];
+weasel.preysOn = [pheasant, rat, squirrel, rabbit];
 sikaDeer.preysOn = [grass];
-yellowWolf.preysOn = [ringNeckPheasant, nestRat, squirrel, wildRabbit];
-wildRabbit.preysOn = [grass];
-snake.preysOn = [nestRat, squirrel, wildRabbit];
-eagle.preysOn = [nestRat, ringNeckPheasant, wildRabbit, squirrel];
 squirrel.preysOn = [grass];
-ringNeckPheasant.preysOn = [grass];
-nestRat.preysOn = [grass];
+pheasant.preysOn = [grass];
+rabbit.preysOn = [grass];
+rat.preysOn = [grass];
+
+// 設定天敵
+sikaDeer.isPreyOf = [leopardCat];
+weasel.isPreyOf = [leopardCat];
+rabbit.isPreyOf = [leopardCat, weasel, snake, eagle];
+squirrel.isPreyOf = [weasel, snake, eagle];
+pheasant.isPreyOf = [weasel, eagle];
+rat.isPreyOf = [weasel, snake, eagle];
+grass.isPreyOf = [sikaDeer, rabbit, squirrel, pheasant, rat];
 
 
 // 創建生態系統實例
 const ecosystem = new Ecosystem([
-  stoneLion,
-  sikaDeer,
-  yellowWolf,
-  wildRabbit,
-  grass,
-  ringNeckPheasant,
-  nestRat,
-  squirrel,
+  leopardCat,
   snake,
-  eagle,
+  eagle,  
+  weasel,  
+  sikaDeer,
+  rabbit,
+  pheasant,
+  rat,
+  squirrel,
+  grass,  
 ]);
 
 // 模擬生態系統
-for (let i = 0; i < 10; i++) {
+ecosystem.printPopulation();
+
+for (let i = 0; i < 100; i++) {
   ecosystem.update();
   ecosystem.printPopulation();
 
 }
-
+/*
 
 // 定義顏色和折線圖位置
 const colors = ["red", "blue", "green", "orange", "purple", "yellow", "cyan", "magenta", "lime", "pink"];
@@ -69,4 +82,5 @@ for (const species of ecosystem.species) {
 
 // 開始繪製折線圖
 updateChart();
+*/
 
