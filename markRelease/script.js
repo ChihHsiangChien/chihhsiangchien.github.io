c4e1186d02047b200d8b64b57be24360ef1d6adb
Chien, ChihHsiang <pancala@gmail.com>
2024-05-29 11:39:57 +0800

    double click

--- markRelease/script.js
+++ markRelease/script.js
@@ -378,7 +378,21 @@ resetBtn.addEventListener('click', () => {
   //重置蝌蚪
   initializeTadpoles();
 
-    // 更新並顯示訊息
-    updateInfo();
+  // 更新並顯示訊息
+  updateInfo();
 
 });
+
+// 避免iOS雙擊使畫面縮放
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+});
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (e) {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+      e.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
